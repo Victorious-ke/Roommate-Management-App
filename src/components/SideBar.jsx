a13ee5565f029d19d30bb88dc0921e103406c076
Beatrice Wambui <bettyneema2@gmail.com>
2025-09-01 02:44:39 +0300

    the styling disappeared at some point during debugging, fixed

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SideBar = ({ chores }) => {
+  const pendingChores = chores.filter(chore => !chore.isCompleted);
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <h2>Dashboard</h2>
+      </div>
+      <div className="sidebar-links">
+        <Link to="/">Home</Link>
+        <Link to="/chores">
+          Chores <span className="chore-count">{pendingChores.length}</span>
+        </Link>
+        <Link to="/chores/new">Add Chore</Link>
+        <Link to="/roommates">Roommates</Link>
+        <Link to="/events">Events</Link>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
