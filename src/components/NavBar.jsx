a25ac7009dac3656754c3165f7b06fccd65171e1
Beatrice Wambui <bettyneema2@gmail.com>
2025-09-01 01:54:45 +0300

    updated with the styling classnames

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,5 +3,25 @@
 /** Database Interaction: None. */
 
 /** (Child of App.jsx)
-
 It will appear on all pages and is not a parent to any of the other main components. */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <span>Roommate Management App</span>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/chores">Chores</Link>
+        <Link to="/chores/new">Add Chore</Link>
+        <Link to="/roommates">Roommates</Link>
+        <Link to="/events">Events</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+
