96befd711faa9fdf19c546dec99a4120123dd66e
Praxcedes <praxcedeskabeya@gmail.com>
2025-09-01 18:22:20 +0300

    app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// src/App.jsx
+import { Routes, Route, Link } from "react-router-dom";
+import { BillsProvider } from "./context/BillsProvider";
+import Home from "./pages/Home";
+import AddExpense from "./pages/AddExpense";
+import Summary from "./pages/Summary";
+import Expenses from "./pages/Expenses";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <BillsProvider>
+      <nav className="navbar">
+        <h1>Roommate Expense Tracker</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/add">Add Expense</Link></li>
+          <li><Link to="/summary">Summary</Link></li>
+          <li><Link to="/expenses">Expenses</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddExpense />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BillsProvider>
+  );
+}
