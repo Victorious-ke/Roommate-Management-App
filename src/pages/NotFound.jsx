d5344ba72a7d91fb1020344ee6def5124a2ef900
Praxcedes <praxcedeskabeya@gmail.com>
2025-09-01 18:25:06 +0300

    Expense.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div>
-      <h2>404 - Page Not Found 🚫</h2>
+      <h2>404 - Page Not Found </h2>
       <p>The page you are looking for doesn’t exist.</p>
       <Link to="/">Go Back Home</Link>
     </div>
