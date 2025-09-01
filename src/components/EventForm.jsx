14502fd207ac375cd7495e7b9201979896301a12
LabanMugutu <labanmugutu@gmail.com>
2025-09-01 13:05:37 +0300

    Creates Form for creating/editing events

---
+++ src/components/EventForm.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+// 🔹 Form used for creating/updating events
+export default function EventForm({ event, date, onClose, refreshEvents }) {
+  const [chores, setChores] = useState([]);
+  const [bills, setBills] = useState([]);
+  // Local form state
+  const [form, setForm] = useState({
+    title: event?.title || "",
+    description: event?.description || "",
+    startTime: event?.start || date || new Date().toISOString(),
+    endTime: event?.end || date || new Date().toISOString(),
+    relatedChoreId: event?.relatedChoreId || "",
+    relatedBillId: event?.relatedBillId || "",
+  });
